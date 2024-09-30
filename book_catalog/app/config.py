c4b2ba83ec499346e248dda254f1c2b7da0aa445
AMG786 <abdulmueezgujjar@gmail.com>
2024-09-30 20:37:41 +1000

    testing code

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -3,4 +3,4 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
 
 # Add a hardcoded secret
-SECRET_KEY = "mysecretkey"  # Hardcoded secret key
+SECRET_KEY = "mysecretkey"  # Hardcoded secret key v
