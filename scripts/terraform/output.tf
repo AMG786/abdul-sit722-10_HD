9c6dd56c42cb6e6d80a6c9385e9ccd773aecbc04
AMG786 <abdulmueezgujjar@gmail.com>
2024-10-03 13:43:39 +1000

    test 3nd COMMIT

---
+++ scripts/terraform/output.tf
@@ -0,0 +1,34 @@
+output "kubeconfig" {
+  value       = azurerm_kubernetes_cluster.cluster.kube_admin_config[0]
+  description = "The kubeconfig file for the AKS cluster."
+}
+
+output "container_registry" {
+  value       = azurerm_container_registry.container_registry.login_server
+  description = "The URL of the Azure Container Registry."
+}
+
+output "registry_username" {
+  value       = azurerm_container_registry.container_registry.admin_username
+  description = "The username for the Azure Container Registry."
+}
+
+output "registry_password" {
+  value       = azurerm_container_registry.container_registry.admin_password
+  description = "The password for the Azure Container Registry."
+}
+
+output "aks_cluster_name" {
+  value       = azurerm_kubernetes_cluster.cluster.name
+  description = "The name of the Azure Kubernetes Service cluster."
+}
+
+output "resource_group_name" {
+  value       = azurerm_resource_group.bmdksub1.name
+  description = "The name of the resource group."
+}
+
+output "location" {
+  value       = azurerm_resource_group.bmdksub1.location
+  description = "The Azure region where the resources are deployed."
+}
