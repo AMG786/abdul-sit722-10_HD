6761ea8c3a1949ddbddab33c70c86d59d5bde5f2
AMG786 <abdulmueezgujjar@gmail.com>
2024-10-03 13:47:38 +1000

    test 3nd COMMIT

--- scripts/terraform/output.tf
+++ scripts/terraform/output.tf
@@ -10,25 +10,30 @@ output "container_registry" {
 
 output "registry_username" {
   value       = azurerm_container_registry.container_registry.admin_username
+  sensitive   = true
   description = "The username for the Azure Container Registry."
 }
 
 output "registry_password" {
   value       = azurerm_container_registry.container_registry.admin_password
+  sensitive   = true
   description = "The password for the Azure Container Registry."
 }
 
 output "aks_cluster_name" {
   value       = azurerm_kubernetes_cluster.cluster.name
+  sensitive   = true
   description = "The name of the Azure Kubernetes Service cluster."
 }
 
 output "resource_group_name" {
   value       = azurerm_resource_group.bmdksub1.name
+  sensitive   = true
   description = "The name of the resource group."
 }
 
 output "location" {
   value       = azurerm_resource_group.bmdksub1.location
+  sensitive   = true
   description = "The Azure region where the resources are deployed."
 }
