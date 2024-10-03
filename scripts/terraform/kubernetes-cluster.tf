7ae64a79c8437289aebf39108d7cd36887b450cb
AMG786 <abdulmueezgujjar@gmail.com>
2024-10-03 20:26:31 +1000

    test 3nd COMMI

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -22,10 +22,9 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 #
 # Attaches the container registry to the cluster.
 #
-# resource "azurerm_role_assignment" "role_assignment" {
-#   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
-#   role_definition_name             = "AcrPull"
-#   scope                            = azurerm_container_registry.container_registry.id
-#   skip_service_principal_aad_check = true
-# }
+ resource "azurerm_role_assignment" "role_assignment" {
+   principal_id                     = azurerm_kubernetes_cluster.cluster.kubelet_identity[0].object_id
+   role_definition_name             = "AcrPull"
+   scope                            = azurerm_container_registry.container_registry.id
+ }
 
