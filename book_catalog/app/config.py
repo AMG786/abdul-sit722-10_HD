453fd34a38163552c187b575a8e17318c512657f
AMG786 <abdulmueezgujjar@gmail.com>
2024-10-05 15:33:47 +1000

    Commit

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -3,7 +3,7 @@ import os
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
 
 # Add a hardcoded secret
-SECRET_KEY = "mysecretkey"  # Hardcoded secret key v
+#SECRET_KEY = "mysecretkey"  # Hardcoded secret key v
 
 # Retrieve the secret key from the environment
-#SECRET_KEY = os.getenv("SECRET_KEY", "fallbacksecret")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallbacksecret")
