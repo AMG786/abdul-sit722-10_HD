593e1a99e5dab1afcd135341f77acdb4559f8301
AMG786 <abdulmueezgujjar@gmail.com>
2024-10-06 16:26:50 +1100

    Commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ az aks get-credentials --resource-group $AZURE_RG --name $AZURE_AKS --overwrite-
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f - 
 
 echo "Deployment. Rolling back."
-#kubectl set image deployment/book-catalog-deployment book-catalog-service=abdulmueez822so.azurecr.io/book-catalog:$VERSION
-#kubectl rollout status deployment/book-catalog-deployment
+kubectl set image deployment/book-catalog-deployment book-catalog-service=abdulmueez823so.azurecr.io/book-catalog:$VERSION
+kubectl rollout status deployment/book-catalog-deployment
 
 # Wait for the deployment to finish
 #kubectl rollout status deployment/book-catalog-deployment || {
--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -1,7 +1,7 @@
 # Sets global variables for this Terraform project.
 
 variable "app_name" {
-  default = "abdulmueez823so"
+  default = "abdulmueez824so"
 }
 
 variable "location" {
